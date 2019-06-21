88f8575dccd1fc61fd65eced0692bde0b3a13070
techbeck015413 <techbeck015413@gmail.com>
2019-06-21 10:43:18 +0800

    Create test02_20190619.py

---
+++ test02_20190619.py
@@ -0,0 +1,32 @@
+#資料：程式的基本單位
+#數字
+
+#字串 "" 內可以表達任意文字
+"測試中文"
+"Hello World"
+
+#布林值
+True
+False
+#有順序、可動的列表List
+[3,4,5]
+["Hello","World"]
+#有順序、不可動的列表Tuple
+(3,4,5)
+("Hello","World")
+#集合(集合沒有順序的概念)
+{3,4,5}
+
+#字典Dictionary
+{"apple":"蘋果","data":"資料"}
+
+#變數:用來儲存資料的自訂名稱
+#變數名稱=資料
+x=4
+# print(資料)
+print(3)
+print(True)
+print([1,2,3])
+print(x)
+x=True #取代舊的資料
+print(x)
